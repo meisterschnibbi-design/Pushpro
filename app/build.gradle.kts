503ade1f26303e7da4638dfd66c5d6e63df3fdb2
meisterschnibbi-design <meister.schnibbi@gmail.com>
2025-09-24 15:04:41 +0200

    Übernommene Mail-Fixes aus ZIP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,8 @@ android {
   }
 }
 dependencies {
-  implementation("com.sun.mail:jakarta.mail:2.0.3")
+  implementation("com.sun.mail:android-mail:1.6.7")
+  implementation("com.sun.mail:android-activation:1.6.7")
   implementation("androidx.core:core-ktx:1.13.1")
   implementation("androidx.appcompat:appcompat:1.7.0")
   implementation("com.google.android.material:material:1.12.0")
--- app/src/main/java/pro/pushpro/app/ui/EmailSettingsActivity.kt
+++ app/src/main/java/pro/pushpro/app/ui/EmailSettingsActivity.kt
@@ -6,12 +6,12 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import pro.pushpro.app.R
 import pro.pushpro.app.util.LogUtil
-import jakarta.mail.Message
-import jakarta.mail.PasswordAuthentication
-import jakarta.mail.Session
-import jakarta.mail.Transport
-import jakarta.mail.internet.InternetAddress
-import jakarta.mail.internet.MimeMessage
+import javax.mail.Message
+import javax.mail.PasswordAuthentication
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 import java.util.Properties
 
 class EmailSettingsActivity : AppCompatActivity() {
@@ -93,7 +93,7 @@ class EmailSettingsActivity : AppCompatActivity() {
 
                 var ok = false
                 try {
-                    val session = Session.getInstance(props, object: jakarta.mail.Authenticator() {
+                    val session = Session.getInstance(props, object: javax.mail.Authenticator() {
                         override fun getPasswordAuthentication() =
                             PasswordAuthentication(userV, passV)
                     })
