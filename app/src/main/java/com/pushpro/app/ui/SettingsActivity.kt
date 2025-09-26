aba3ed6daa17c6dfad0e5a4617d82821dcbfcf8a
meisterschnibbi-design <meister.schnibbi@gmail.com>
2025-09-26 12:16:58 +0200

    project_fixed_ready_v13.zip

--- app/src/main/java/com/pushpro/app/ui/SettingsActivity.kt
+++ app/src/main/java/com/pushpro/app/ui/SettingsActivity.kt
@@ -1,12 +1,12 @@
-package com.pushpro.ui.settings
+package com.pushpro.app.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pushpro.R
 
-class TelegramSettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_telegram_settings)
+        
     }
 }
--- app/src/main/java/com/pushpro/ui/settings/WebhookSettingsActivity.kt
+++ app/src/main/java/com/pushpro/ui/settings/WebhookSettingsActivity.kt
@@ -8,15 +8,5 @@ class WebhookSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webhook_settings)
-
-        findViewById<android.view.View>(R.id.rootWebhook)
-        findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.inputUrl)
-        findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.inputHttpMethod)
-        findViewById<android.widget.Spinner>(R.id.spinnerMethod)
-        findViewById<android.widget.Spinner>(R.id.spinnerContentType)
-        findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.inputHeaders)
-        findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.inputTemplate)
-        findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.inputJsonTemplate)
-        findViewById<com.google.android.material.button.MaterialButton>(R.id.btnSendTest)
     }
 }
